997d65e6e029d27f2ab29424ea6c48090921a721
keegAnO918 <keegan.ong27@ycdsbk12.ca>
2026-05-28 14:01:08 -0400

    Fix too many blocks overlapping

--- src/Sketch.java
+++ src/Sketch.java
@@ -31,10 +31,11 @@ public class Sketch extends PApplet {
     }
     /**
      * Creates multiple rectangles from 0-9 and gives it a random x and y value. The -400 y is so the blocks spawn at different heights and fall at different levels
+     * Width / 40 makes it so there's columns. (600/40 = 15) Then once it generates a random number (3, 4, 2, 5, 15, etc) it'll * it by 40 to fit the entire width
      */
     private void makeRectangle() {
          for (int rectMultiple = 0; rectMultiple < rectX.length; rectMultiple++) {
-            rectX[rectMultiple] = (int) random(0, 440);
+            rectX[rectMultiple] = (int) random(0, width / 40) * 40;
             rectY[rectMultiple] = (int) random(-1500, 0);
         }
     }
