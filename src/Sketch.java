e5405823efb41bf570e328319f62a8aaeb315e1d
chi0kenNugg1es <brianna.tsang09@gmail.com>
2026-05-29 12:31:57 -0400

    Added the Cauldron - Brianna

--- src/Sketch.java
+++ src/Sketch.java
@@ -21,26 +21,31 @@ public class Sketch extends PApplet {
 
     @Override
     public void setup() {
-        makeRectangle(); // In setup to help get each rectangle from the array
+        rectangleSpawnLocation(); // In setup to help get each rectangle from the array
     }
 
     @Override
     public void draw() {
         background(244);
         fallingRectangle(3);
+        cauldron(300,350);
 
     }
     /**
      * Creates multiple rectangles from 0-9 and gives it a random x and y value. The -400 y is so the blocks spawn at different heights and fall at different levels
      * Width / 40 makes it so there's columns. (600/40 = 15) Then once it generates a random number (3, 4, 2, 5, 15, etc) it'll * it by 40 to fit the entire width
      */
-    private void makeRectangle() {
+    private void rectangleSpawnLocation() {
+        fill(23, 23, 32);
          for (int rectMultiple = 0; rectMultiple < rectX.length; rectMultiple++) {
+
+
             rectX[rectMultiple] = (int) random(0, width / 40) * 40;
             rectY[rectMultiple] = (int) random(-1500, 0);
         }
     }
     private void fallingRectangle(int speed) {
+        fill(23,23,23);
         for (int rectMultiple = 0; rectMultiple < rectX.length; rectMultiple++){
             rect(rectX[rectMultiple], rectY[rectMultiple], 40, 20);
             rectY[rectMultiple] += speed;
@@ -48,4 +53,23 @@ public class Sketch extends PApplet {
 
 
     }
+
+    public void cauldron(int x, int y)
+    {
+        //pot
+        noStroke();
+        fill(42, 40, 46);
+        circle(x,y,100);
+        ellipse(x,y-40,120, 30);
+        //content
+        noStroke();
+        fill(250,250,250);
+        ellipse(x, y-40, 90, 20);
+    }
+
+    public void moveCauldron()
+    {
+            if(keyPre)
+    }
+
 }
