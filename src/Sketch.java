c3cc27bfb3a6200def0486d00ed6450ef7cbbb1d
keegAnO918 <keegan.ong27@ycdsbk12.ca>
2026-06-01 13:45:08 -0400

    Add score for when rectangle enter the Cauldron

--- src/Sketch.java
+++ src/Sketch.java
@@ -7,10 +7,11 @@ import processing.core.PApplet;
  */
 public class Sketch extends PApplet {
 
-    int[] rectX = new int [70];
-    int[] rectY = new int [70];
+    int[] rectX = new int [10];
+    int[] rectY = new int [10];
     int cauldronX = 300;
     int cauldronY = 350;
+    int score = 0;
 
     public static void main(String[] args) {
         PApplet.main("Sketch");
@@ -32,6 +33,7 @@ public class Sketch extends PApplet {
         fallingRectangle(3);
         cauldron();
         moveCauldron();
+        collectObjects();
 
 
     }
@@ -39,6 +41,9 @@ public class Sketch extends PApplet {
      * Creates multiple rectangles from 0-9 and gives it a random x and y value. The -400 y is so the blocks spawn at different heights and fall at different levels
      * Width / 40 makes it so there's columns. (600/40 = 15) Then once it generates a random number (3, 4, 2, 5, 15, etc) it'll * it by 40 to fit the entire width
      */
+
+    
+
     private void rectangleSpawnLocation() {
         fill(23, 23, 32);
          for (int rectMultiple = 0; rectMultiple < rectX.length; rectMultiple++) {
@@ -51,12 +56,32 @@ public class Sketch extends PApplet {
     private void fallingRectangle(int speed) {
         fill(23,23,23);
         for (int rectMultiple = 0; rectMultiple < rectX.length; rectMultiple++){
-            rect(rectX[rectMultiple], rectY[rectMultiple], 40, 20);
+            rect(rectX[rectMultiple], rectY[rectMultiple], 40, 20); // creates the rectangle
             rectY[rectMultiple] += speed;
         }
 
 
     }
+
+    public void collectObjects() {
+        for (int rectMultiple = 0; rectMultiple < rectX.length; rectMultiple++) {
+            // Must be within a specific y coordinate at all times, and mnust be within the caludron's bondaries hence the -60, +60
+            if (rectY[rectMultiple] >= 290 && rectY[rectMultiple] <= 320 && rectX[rectMultiple] >= cauldronX - 60 && rectX[rectMultiple] <= cauldronX + 60) {
+
+                score++;
+
+                // Respawn rectangle at top so it doesn't continue falling
+                rectY[rectMultiple] = (int) random(-1500, 0);
+                rectX[rectMultiple] = (int) random(0, width / 40) * 40;
+
+            }
+        }
+
+        fill(0);
+        textSize(20);
+        text("Score: " + score, 20, 30);
+}
+    
 
     public int cauldron( )
     {
@@ -65,24 +90,24 @@ public class Sketch extends PApplet {
         noStroke();
         fill(42, 40, 46);
         circle(cauldronX,cauldronY,100);
-        ellipse(cauldronX,cauldronY-40,120, 30);
+        ellipse(cauldronX, 310, 120, 30);
         //content
         noStroke();
         fill(250,250,250);
-        ellipse(cauldronX, cauldronY-40, 90, 20);
+        ellipse(cauldronX, 310, 90, 20);
         return cauldronX;
     }
 
     public void moveCauldron()
     {
-            if(keyPressed && (key == 'a' || key == CODED && keyCode == LEFT) && cauldronX >= 50)
+            if(keyPressed && key == 'a'  && cauldronX >= 50)
             {
-                cauldronX -= 5;
+                cauldronX -= 10;
             }
 
-            if(keyPressed && (key == 'd' || key == CODED && keyCode == RIGHT) && cauldronX <= 550)
+            if(keyPressed && key == 'd'  && cauldronX <= 550)
             {
-                cauldronX += 5;
+                cauldronX += 10;
             }
     }
 
